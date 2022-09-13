a38229728993a427dc24346094db3981af7a7f5f
won.develop <won.develop@gmail.com>
2022-09-14 00:54:36 +0900

    add rollup config on package.json

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from "rollup-plugin-dts";
+import packageJson from './package.json';
+
+const plugins = [
+  resolve(),
+  typescript({ tsconfig: './tsconfig.json' }),
+  commonjs({
+    ignoreGlobal: true,
+    include: /\/node_modules\//,
+  }),
+];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: plugins,
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];

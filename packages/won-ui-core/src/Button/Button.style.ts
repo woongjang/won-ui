aa84f377cecf23e3e6028571271da6a3f407e9fe
WON <won.develop@gmail.com>
2022-10-26 00:28:32 +0900

    [@won-ui/core] Button 스타일 추가

--- packages/won-ui-core/src/Button/Button.style.ts
+++ packages/won-ui-core/src/Button/Button.style.ts
@@ -2,6 +2,21 @@ import { css } from '@emotion/react';
 
 export const button = css`
   font-family: Charcoal, sans-serif;
-  border: 1px solid transparent;
+  border: none;
   outline: 0;
+  padding: 0 15px;
+  height: 30px;
+  border-radius: 6px;
+  font-weight: bold;
+  font-size: 12px;
+  cursor: pointer;
+  user-select: none;
+  letter-spacing: 2px;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.1);
+  transition: all 0.1s ease 0s;
+  
+  &:active {
+    box-shadow: 0px 3px rgba(0, 0, 0, 0.1);
+    transform: translateY(3px);
+  }
 `;

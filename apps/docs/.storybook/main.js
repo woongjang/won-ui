b59eba7b97fd5e7311ef609721b9f91bf3959265
WON <won.develop@gmail.com>
2022-10-26 00:09:49 +0900

    [won-ui] emotion/react storybook에서 적용되도록 수정

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,24 +1,36 @@
-const path = require("path");
+const path = require('path');
+const react = require('@vitejs/plugin-react');
 
 module.exports = {
-  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/react",
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
   async viteFinal(config, { configType }) {
-    // customize the Vite config here
     return {
       ...config,
+      esbuild: {
+        logOverride: { "this-is-undefined-in-esm": "silent" },
+      },
+      plugins: [
+        ...config.plugins.filter(
+          plugin => !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react'))
+        ),
+        react({
+          exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+          jsxImportSource: '@emotion/react',
+          babel: {
+            plugins: ['@emotion/babel-plugin'],
+          },
+        }),
+      ],
       resolve: {
         alias: [
           {
-            find: "@won-ui/core",
-            replacement: path.resolve(
-              __dirname,
-              "../../../packages/won-ui-core/"
-            ),
+            find: '@won-ui/core',
+            replacement: path.resolve(__dirname, '../../../packages/won-ui-core/'),
           },
         ],
       },

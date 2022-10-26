606031a6edf5d400cf8af1d2ee7d862511904241
WON <won.develop@gmail.com>
2022-10-26 20:41:17 +0900

    [@won-ui/core] <Text> as props 수정

--- packages/won-ui-core/src/Text/Text.tsx
+++ packages/won-ui-core/src/Text/Text.tsx
@@ -1,32 +1,24 @@
-import { css } from '@emotion/react';
 import { createElement, CSSProperties, PropsWithChildren } from 'react';
 import { fontSizes, text } from './Text.style';
 
 export interface TextProps {
   className?: string;
   style?: CSSProperties;
-  as?: string;
+  as?: keyof JSX.IntrinsicElements;
   // material ui 와 같은 사이즈 목록 제공
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 
 export function Text({
   className,
-  as = 'p',
+  as: Component = 'p',
   size = 'md',
   children,
   ...restProps
 }: PropsWithChildren<TextProps>) {
-  return createElement(
-    as,
-    {
-      className: className,
-      css: css`
-        ${text}
-        ${fontSizes[size]}
-      `,
-      ...restProps,
-    },
-    children
+  return (
+    <Component className={className} css={[text, fontSizes[size]]} {...restProps}>
+      {children}
+    </Component>
   );
 }

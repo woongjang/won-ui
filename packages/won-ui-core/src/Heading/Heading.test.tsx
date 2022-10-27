9e1e3823d4271e500ad69cca2ae3839c8eea3315
WON <won.develop@gmail.com>
2022-10-27 18:33:45 +0900

    [@won-ui/core] <Heading> init Heading test

---
+++ packages/won-ui-core/src/Heading/Heading.test.tsx
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Heading, HeadingProps } from './Heading';
+
+describe('Heading', () => {
+  const testId = 'won-ui-heading';
+  it('[rendering] Heading 컴포넌트 렌더링', () => {
+    render(<Heading data-testid={testId}>test</Heading>);
+    expect(screen.getByTestId(testId)).toBeInTheDocument();
+  });
+
+  it.each<HeadingProps['level']>([1, 2, 3])(
+    '[level] Heading 컴포넌트 level(h1, h2, ...) 테스트',
+    level => {
+      const { rerender } = render(
+        <Heading level={level} data-testid={testId}>
+          h{level}
+        </Heading>
+      );
+      if (level === 1) {
+        // 기본값 테스트
+        rerender(<Heading data-testid={testId}>h{level}</Heading>);
+        expect(screen.getByTestId(testId).tagName).toBe('H1');
+      }
+      expect(screen.getByTestId(testId).tagName).toBe(`H${level}`);
+    }
+  );
+});

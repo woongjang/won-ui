98aca8e7a945cefb72c056e7c7741443ba6e3ba1
WON <won.develop@gmail.com>
2022-10-27 18:20:46 +0900

    [@won-ui/core] <Button> init Button test

--- packages/won-ui-core/src/Button/Button.test.tsx
+++ packages/won-ui-core/src/Button/Button.test.tsx
@@ -1,10 +1,46 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
+import { Button, ButtonProps } from './Button';
+
+/**
+ * 각 테스트 메시지
+ * [주요 테스트 사항] 세부 설명
+ */
 
 describe('Button', () => {
-  test('버튼 렌더링 테스트', () => {
-    render(<Button data-testid="won-ui-button" />);
-    expect(screen.getByTestId('won-ui-button')).toBeInTheDocument();
+  const testId = 'won-ui-button';
+  it('[rendering] Button 컴포넌트 렌더링', () => {
+    render(<Button data-testid={testId} />);
+    expect(screen.getByTestId(testId)).toBeInTheDocument();
+  });
+
+  it('[children] children props 지원', () => {
+    render(
+      <Button>
+        <span>children</span>
+      </Button>
+    );
+    expect(screen.getByText('children')).toBeInTheDocument();
+  });
+
+  it.each<ButtonProps['type']>(['button', 'submit', 'reset'])('[type] type props 지원', type => {
+    render(
+      <Button type={type} data-testid={testId}>
+        test
+      </Button>
+    );
+    expect(screen.getByTestId(testId)).toHaveAttribute('type', type);
+  });
+
+  it('[disabled] disabled 적용 여부', () => {
+    const { rerender } = render(<Button data-testid={testId}>test</Button>);
+    expect(screen.getByTestId(testId)).not.toHaveAttribute('disabled');
+
+    rerender(
+      <Button disabled data-testid={testId}>
+        test
+      </Button>
+    );
+    expect(screen.getByTestId(testId)).toHaveAttribute('disabled');
   });
 });

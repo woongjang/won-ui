a0bee66aef1523a73b1171684cb2033dfab53f31
WON <won.develop@gmail.com>
2022-11-02 13:07:43 +0900

    [@won-ui/core] <Button> color 적용

--- packages/won-ui-core/src/Button/Button.tsx
+++ packages/won-ui-core/src/Button/Button.tsx
@@ -1,21 +1,30 @@
+import { wonColor } from '@won-ui/styles';
 import { ButtonHTMLAttributes, CSSProperties, PropsWithChildren } from 'react';
-import { button } from './Button.style';
+import { buttonStyle } from './Button.style';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   style?: CSSProperties;
   disabled?: boolean;
+  color?: keyof typeof wonColor;
 }
 
 export function Button({
   className,
   type = 'button',
   disabled = false,
+  color = 'blue',
   children,
   ...restProps
 }: PropsWithChildren<ButtonProps>) {
   return (
-    <button className={className} css={button} disabled={disabled} type={type} {...restProps}>
+    <button
+      className={className}
+      css={buttonStyle(color)}
+      disabled={disabled}
+      type={type}
+      {...restProps}
+    >
       {children}
     </button>
   );

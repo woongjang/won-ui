b5bf3bb4fc5e3bd1f0747badcbebfc366e5c0e89
WON <won.develop@gmail.com>
2022-11-24 17:16:28 +0900

    [@won-ui/core] <Button> variant 속성 추가

--- packages/won-ui-core/src/Button/Button.style.ts
+++ packages/won-ui-core/src/Button/Button.style.ts
@@ -1,39 +1,70 @@
-import { css } from '@emotion/react';
+import { css, SerializedStyles } from '@emotion/react';
 import { black, wonColor } from '@won-ui/styles';
-import { Colors } from '@won-ui/types';
+import { Colors, WonColor } from '@won-ui/types';
 
-export const buttonStyle = (theme: Colors) => {
-  const color = wonColor[theme];
-  return css`
-    box-sizing: border-box;
-    font-family: Charcoal, sans-serif;
-    border: none;
-    outline: 0;
-    padding: 0 15px;
-    height: 30px;
-    border-radius: 6px;
-    font-weight: bold;
-    font-size: 14px;
-    cursor: pointer;
-    user-select: none;
-    letter-spacing: 2px;
-    box-shadow: 0px 3px 3px ${color[20]};
-    transition: all 0.1s ease 0s;
+export type ButtonVariant = 'filled' | 'light' | 'outline' | 'borderless';
 
-    background-color: ${color[60]};
-    color: ${color[10]};
-
-    &:active {
-      box-shadow: 0px 3px rgba(0, 0, 0, 0.1);
-      transform: translateY(3px);
-    }
-
-    &:disabled {
+const getVariantStyle = (color: WonColor, variant: ButtonVariant) => {
+  const variants: { [variant in ButtonVariant]: SerializedStyles } = {
+    filled: css`
+      border: 2px solid transparent;
+      box-shadow: 0px 3px 3px ${color[20]};
+      background-color: ${color[60]};
+      color: ${color[10]};
+    `,
+    light: css`
+      border: 2px solid transparent;
+      box-shadow: 0px 3px 3px ${color[20]};
+      background-color: ${color[10]};
+      color: ${color[60]};
+    `,
+    outline: css`
+      border: 2px solid ${color[60]};
+      box-shadow: 0px 3px 3px ${color[20]};
+      background-color: transparent;
+      color: ${color[60]};
+    `,
+    borderless: css`
+      border: 2px solid transparent;
+      background-color: transparent;
       box-shadow: none;
-      transform: none;
-      pointer-events: none;
-      color: ${black[20]};
-      background-color: ${black[10]};
-    }
-  `;
+      color: ${color[60]};
+    `,
+  };
+  return variants[variant];
+};
+
+export const buttonStyle = (theme: Colors, variant: ButtonVariant) => {
+  const variantStyle = getVariantStyle(wonColor[theme], variant);
+  return [
+    variantStyle,
+    css`
+      box-sizing: border-box;
+      font-family: Charcoal, sans-serif;
+      outline: 0;
+      padding: 0 15px;
+      height: 30px;
+      border-radius: 6px;
+      font-weight: bold;
+      font-size: 14px;
+      cursor: pointer;
+      user-select: none;
+      letter-spacing: 0.5px;
+      transition: all 0.1s ease 0s;
+
+      &:active {
+        box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.1);
+        transform: translateY(3px);
+      }
+
+      &:disabled {
+        border: none;
+        box-shadow: none;
+        transform: none;
+        pointer-events: none;
+        color: ${black[20]};
+        background-color: ${black[10]};
+      }
+    `,
+  ];
 };

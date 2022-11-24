c488b7a287f03c057d9a92f6082933259de54da6
WON <won.develop@gmail.com>
2022-11-24 17:16:28 +0900

    [@won-ui/core] <Button> variant 속성 추가

--- apps/docs/stories/core/Button.stories.tsx
+++ apps/docs/stories/core/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 import { ComponentMeta } from '@storybook/react';
-import { Button, Stack } from '@won-ui/core/src';
+import { Button, Stack, Text } from '@won-ui/core/src';
 
 export default {
   title: 'won-ui/core/Button',
@@ -9,31 +9,107 @@ export default {
 
 export const Basic = () => {
   return (
-    <Stack
-      direction='column'
-      align="flex-start"
-      css={css`
-        width: 100px;
-        > * {
-          margin-bottom: 10px;
-        }
-      `}
-    >
-      <Button>
-        Basic
-      </Button>
-      <Button disabled>Disabled</Button>
-      <Button color="black">black</Button>
-      <Button color="red">red</Button>
-      <Button color="orange">orange</Button>
-      <Button color="magenta">magenta</Button>
-      <Button color="yellow">yellow</Button>
-      <Button color="blue">blue</Button>
-      <Button color="cyan">cyan</Button>
-      <Button color="indigo">indigo</Button>
-      <Button color="purple">purple</Button>
-      <Button color="green">green</Button>
-      <Button color="teal">teal</Button>
+    <Stack direction="column" align="flex-start">
+      <Text size="lg" as="span">
+        Filled
+      </Text>
+      <Stack
+        direction="row"
+        align="flex-start"
+        css={css`
+          flex-wrap: wrap;
+          padding: 20px 0;
+        `}
+      >
+        <Button>Basic</Button>
+        <Button disabled>Disabled</Button>
+        <Button color="black">black</Button>
+        <Button color="red">red</Button>
+        <Button color="orange">orange</Button>
+        <Button color="magenta">magenta</Button>
+        <Button color="yellow">yellow</Button>
+        <Button color="blue">blue</Button>
+        <Button color="cyan">cyan</Button>
+        <Button color="indigo">indigo</Button>
+        <Button color="purple">purple</Button>
+        <Button color="green">green</Button>
+        <Button color="teal">teal</Button>
+      </Stack>
+      <Text size="lg" as="span">
+        light
+      </Text>
+      <Stack
+        direction="row"
+        align="flex-start"
+        css={css`
+          flex-wrap: wrap;
+          padding: 20px 0;
+        `}
+      >
+        <Button variant="light">Basic</Button>
+        <Button variant="light" disabled>Disabled</Button>
+        <Button variant="light" color="black">black</Button>
+        <Button variant="light" color="red">red</Button>
+        <Button variant="light" color="orange">orange</Button>
+        <Button variant="light" color="magenta">magenta</Button>
+        <Button variant="light" color="yellow">yellow</Button>
+        <Button variant="light" color="blue">blue</Button>
+        <Button variant="light" color="cyan">cyan</Button>
+        <Button variant="light" color="indigo">indigo</Button>
+        <Button variant="light" color="purple">purple</Button>
+        <Button variant="light" color="green">green</Button>
+        <Button variant="light" color="teal">teal</Button>
+      </Stack>
+      <Text size="lg" as="span">
+        outline
+      </Text>
+      <Stack
+        direction="row"
+        align="flex-start"
+        css={css`
+          flex-wrap: wrap;
+          padding: 20px 0;
+        `}
+      >
+        <Button variant="outline" >Basic</Button>
+        <Button variant="outline" disabled>Disabled</Button>
+        <Button variant="outline" color="black">black</Button>
+        <Button variant="outline" color="red">red</Button>
+        <Button variant="outline" color="orange">orange</Button>
+        <Button variant="outline" color="magenta">magenta</Button>
+        <Button variant="outline" color="yellow">yellow</Button>
+        <Button variant="outline" color="blue">blue</Button>
+        <Button variant="outline" color="cyan">cyan</Button>
+        <Button variant="outline" color="indigo">indigo</Button>
+        <Button variant="outline" color="purple">purple</Button>
+        <Button variant="outline" color="green">green</Button>
+        <Button variant="outline" color="teal">teal</Button>
+      </Stack>
+      <Text size="lg" as="span">
+        borderless
+      </Text>
+      <Stack
+        direction="row"
+        align="flex-start"
+        css={css`
+          flex-wrap: wrap;
+          padding: 20px 0;
+        `}
+      >
+        <Button variant="borderless" >Basic</Button>
+        <Button variant="borderless" disabled>Disabled</Button>
+        <Button variant="borderless" color="black">black</Button>
+        <Button variant="borderless" color="red">red</Button>
+        <Button variant="borderless" color="orange">orange</Button>
+        <Button variant="borderless" color="magenta">magenta</Button>
+        <Button variant="borderless" color="yellow">yellow</Button>
+        <Button variant="borderless" color="blue">blue</Button>
+        <Button variant="borderless" color="cyan">cyan</Button>
+        <Button variant="borderless" color="indigo">indigo</Button>
+        <Button variant="borderless" color="purple">purple</Button>
+        <Button variant="borderless" color="green">green</Button>
+        <Button variant="borderless" color="teal">teal</Button>
+      </Stack>
     </Stack>
   );
 };
--- packages/won-ui-core/src/Button/Button.tsx
+++ packages/won-ui-core/src/Button/Button.tsx
@@ -1,12 +1,13 @@
 import { Colors } from '@won-ui/types';
 import { ButtonHTMLAttributes, CSSProperties, PropsWithChildren } from 'react';
-import { buttonStyle } from './Button.style';
+import { buttonStyle, ButtonVariant } from './Button.style';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   style?: CSSProperties;
   disabled?: boolean;
   color?: Colors;
+  variant?: ButtonVariant;
 }
 
 export function Button({
@@ -15,12 +16,13 @@ export function Button({
   disabled = false,
   color = 'blue',
   children,
+  variant = 'filled',
   ...restProps
 }: PropsWithChildren<ButtonProps>) {
   return (
     <button
       className={className}
-      css={buttonStyle(color)}
+      css={buttonStyle(color, variant)}
       disabled={disabled}
       type={type}
       {...restProps}

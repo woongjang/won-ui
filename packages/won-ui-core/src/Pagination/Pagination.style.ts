9d0cba93865e2710c1d35a15bca0792c3320441a
WON <won.develop@gmail.com>
2022-11-25 16:12:21 +0900

    [@won-ui/core] <Pagination> div Container => Stack 변경

--- packages/won-ui-core/src/Pagination/Pagination.style.ts
+++ packages/won-ui-core/src/Pagination/Pagination.style.ts
@@ -1,17 +1,6 @@
 import { css } from '@emotion/react';
-import { Colors } from '@won-ui/types';
 
-export const containerStyle = css`
-  display: flex;
-  justify-content: center;
-`;
-
-export const paginationStyle = css`
-  display: flex;
-  justify-self: center;
-`;
-
-export const arrowStyle = (theme: Colors) => ({
+export const arrowStyle = {
   leftArrow: css`
     display: flex;
     align-items: center;
@@ -24,7 +13,7 @@ export const arrowStyle = (theme: Colors) => ({
     padding: 0 3px;
     margin-left: 5px;
   `,
-});
+};
 
 export const pageBtnStyle = (isSelected: boolean) => css`
   margin-right: 2px;

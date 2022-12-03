dd2e6b083e8463d782f40a7253f1eaf0fdbaa4c2
WON <won.develop@gmail.com>
2022-12-03 21:44:18 +0900

    [@won-ui/core] <Pagination> hasMoreButton 기능 추가

--- packages/won-ui-core/src/Pagination/Pagination.tsx
+++ packages/won-ui-core/src/Pagination/Pagination.tsx
@@ -1,9 +1,9 @@
 import { Colors } from '@won-ui/types';
-import { CaretLeft, CaretRight } from 'phosphor-react';
+import { CaretLeft, CaretRight, DotsThree } from 'phosphor-react';
 import { ChangeEvent, MouseEvent, useMemo, useState } from 'react';
 import { Button } from '../Button/Button';
 import { Stack } from '../Stack/Stack';
-import { arrowStyle, pageBtnStyle } from './Pagination.style';
+import { arrowStyle, moreBtnStyle, pageBtnStyle } from './Pagination.style';
 
 interface PaginationProps {
   defaultCurrent?: number;
@@ -14,13 +14,12 @@ interface PaginationProps {
   pagesGap?: number;
   color?: Colors;
   total: number;
+  hasMoreButton?: boolean;
 }
 
 /**
  * TODO:
  *  - usePagination 추출해서 커스텀 가능하도록 훅 제공
- *  - 페이지 건너뛰기 기능 추가할 것
- *  - 페이지 축약 표현 선택 프롭스로 추가할 것
  */
 export function Pagination(props: PaginationProps) {
   const {
@@ -28,13 +27,14 @@ export function Pagination(props: PaginationProps) {
     currentPage: propsCurrent,
     onChange,
     pageSizeOptions = [10, 20, 30, 50],
-    defaultPageSize: propsPageSize = pageSizeOptions[0],
+    defaultPageSize = pageSizeOptions[0],
     pagesGap = 2,
     total,
     color = 'blue',
+    hasMoreButton = false,
   } = props;
   const [current, setCurrent] = useState(defaultCurrent || 1);
-  const [pageSize, setPageSize] = useState(propsPageSize);
+  const [pageSize, setPageSize] = useState(defaultPageSize);
 
   const page = useMemo(() => (propsCurrent ? propsCurrent : current), [propsCurrent, current]);
 
@@ -47,9 +47,48 @@ export function Pagination(props: PaginationProps) {
       startNum = 1;
     }
 
-    const pagesLength = maxPageNum < pagesGap * 2 + 1 ? maxPageNum : pagesGap * 2 + 1;
+    let pagesLength = maxPageNum < pagesGap * 2 + 1 ? maxPageNum : pagesGap * 2 + 1;
+
+    if (hasMoreButton && maxPageNum > pagesGap * 2 + 1) {
+      if (maxPageNum > pagesGap * 2 && current >= maxPageNum - pagesGap * 2) {
+        pagesLength = (pagesGap + 1) * 2 + 1;
+        if (page === maxPageNum - pagesGap * 2) {
+          pagesLength -= 1;
+        }
+        startNum = maxPageNum - (pagesGap + 1) * 2;
+      }
+
+      if (startNum >= 1 && current < (pagesGap + 1) * 2) {
+        startNum = 1;
+        pagesLength = (pagesGap + 1) * 2 + 1;
+      }
+    }
+
     return Array.from({ length: pagesLength }, (_, i) => i + startNum);
   }, [current, total, pageSize, pagesGap, propsCurrent]);
+
+  const showFirstPage = useMemo(
+    () => hasMoreButton && current > pagesGap * 2 + 1,
+    [hasMoreButton, current, pagesGap]
+  );
+
+  const showLastPage = useMemo(
+    () =>
+      hasMoreButton &&
+      Math.ceil(total / pageSize) - pagesGap * 2 > pagesGap * 2 + 1 &&
+      current <= Math.ceil(total / pageSize) - pagesGap * 2,
+    [hasMoreButton, current, total, pagesGap, pageSize]
+  );
+
+  const showLeftMoreButton = useMemo(
+    () => hasMoreButton && current > pagesGap * 2 + 1,
+    [hasMoreButton, current, pagesGap]
+  );
+
+  const showRightMoreButton = useMemo(
+    () => hasMoreButton && current < Math.ceil(total / pageSize) - pagesGap * 2,
+    [hasMoreButton, current, pagesGap, total, pageSize]
+  );
 
   const handleChangePage = (page: number) => (e: MouseEvent<HTMLSpanElement>) => {
     if (onChange) {
@@ -82,8 +121,22 @@ export function Pagination(props: PaginationProps) {
     setCurrent(page + 1);
     if (onChange) onChange(page + 1, pageSize);
   };
+
+  const handleClickMoreButton =
+    (position: 'left' | 'right') => (e: MouseEvent<HTMLButtonElement>) => {
+      if (position === 'left') {
+        setCurrent(prev => (prev - (pagesGap * 2 + 1) <= 1 ? 1 : prev - (pagesGap * 2 + 1)));
+        return;
+      }
+      setCurrent(prev =>
+        prev + pagesGap * 2 + 1 >= Math.ceil(total / pageSize)
+          ? Math.ceil(total / pageSize)
+          : prev + pagesGap * 2 + 1
+      );
+    };
+
   return (
-    <Stack >
+    <Stack>
       <Stack>
         <Button
           css={arrowStyle.leftArrow}
@@ -93,6 +146,26 @@ export function Pagination(props: PaginationProps) {
         >
           <CaretLeft size={16} weight="bold" />
         </Button>
+        {showFirstPage && hasMoreButton && (
+          <Button
+            color={color}
+            css={pageBtnStyle(page === 1)}
+            onClick={handleChangePage(1)}
+            variant={page === 1 ? 'filled' : 'outline'}
+          >
+            {1}
+          </Button>
+        )}
+        {showLeftMoreButton && hasMoreButton && (
+          <Button
+            css={moreBtnStyle}
+            color={color}
+            onClick={handleClickMoreButton('left')}
+            variant="borderless"
+          >
+            <DotsThree size={24} />
+          </Button>
+        )}
         {currentPages.map(el => (
           <Button
             color={color}
@@ -104,6 +177,26 @@ export function Pagination(props: PaginationProps) {
             {el}
           </Button>
         ))}
+        {showRightMoreButton && hasMoreButton && (
+          <Button
+            css={moreBtnStyle}
+            color={color}
+            onClick={handleClickMoreButton('right')}
+            variant="borderless"
+          >
+            <DotsThree size={24} />
+          </Button>
+        )}
+        {showLastPage && hasMoreButton && (
+          <Button
+            color={color}
+            css={pageBtnStyle(page === Math.ceil(total / pageSize))}
+            onClick={handleChangePage(Math.ceil(total / pageSize))}
+            variant={page === Math.ceil(total / pageSize) ? 'filled' : 'outline'}
+          >
+            {Math.ceil(total / pageSize)}
+          </Button>
+        )}
         <Button
           css={arrowStyle.rightArrow}
           color={color}
